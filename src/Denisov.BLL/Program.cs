8fe8f9617475eabf357bfed78585fbf038de4e8d
Viktor <47116698+llmurd0kll@users.noreply.github.com>
2020-06-10 01:53:54 +0300

    feat: Tobacco Store project created

---
+++ src/Denisov.BLL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Denisov.BLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
