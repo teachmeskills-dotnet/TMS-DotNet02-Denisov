f7dd46366ff0a37e33ededce700c39f67c39efec
Viktor <47116698+llmurd0kll@users.noreply.github.com>
2020-06-16 19:17:18 +0300

    feat: README and Startup files updated

--- src/Denisov.TobaccoStore/Startup.cs
+++ src/Denisov.TobaccoStore/Startup.cs
@@ -20,20 +20,16 @@ namespace Denisov.TobaccoStore
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<DenisovDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
-            x => x.MigrationsAssembly("Denisov.DAL")));
+
+            services.AddDbContext<DenisovDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), x => x.MigrationsAssembly("Denisov.DAL")));
+
             services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<DenisovDbContext>();
 
             //services.AddScoped(typeof(IRepository<>), typeof(RepositoryManager<>));
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -43,7 +39,6 @@ namespace Denisov.TobaccoStore
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
