3cc04ba22e3b13094d45ed3a846d2a80c791ee21
Viktor <47116698+llmurd0kll@users.noreply.github.com>
2020-08-18 19:07:07 +0300

    feat: added personal page with disqus. fix: some bugs fixed.

---
+++ src/Denisov.TobaccoStore/Controllers/PersonalPageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Denisov.TobaccoStore.Controllers
+{
+    public class PersonalPageController : Controller
+    {
+        public IActionResult PersonalPage()
+        {
+            return View();
+        }
+    }
+}
